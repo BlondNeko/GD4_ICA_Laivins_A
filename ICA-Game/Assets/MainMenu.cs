918457fa52c2bf9ccf70d2fa95ac583422dfa172
Aleksandrs <axel1002@gmail.com>
2022-02-05 19:40:51 +0000

    Main Menu
    Basic menu functions + graphics

---
+++ ICA-Game/Assets/MainMenu.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // following tutorial from https://www.youtube.com/watch?v=RsgiYqLID-U
+    // button quits game
+    public void QuitButton()
+    {
+        Application.Quit();
+    }
+
+    //button starts first scene
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+}
